dc495baccd973424dc4f1f44deb490fb311242e7
dwreckTheOG <mwabayadaniel9@gmail.com>
2023-10-25 18:33:46 +0300

    palindromee

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -31,7 +31,7 @@ int is_palindrome(char *s)
     int len = _strlen_recursion(s);
     if (len <= 1)
         {
-            return (1);,
+            return (1);
         }
     else if (s[0] != s[len - 1])
     {
